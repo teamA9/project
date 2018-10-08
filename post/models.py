65b707faf112690be57dfc6e3602119c137c6964
surfacefrancis <ruidongde@yeah.net>
2018-10-08 17:21:58 +0800

    updata

--- post/models.py
+++ post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=64, verbose_name='标题')
+    field = models.TextField(verbose_name='正文')
+
+
+
+
+
+
+
+
+
+
+
--- post/views.py
+++ post/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from models import Post
 
 # Create your views here.
+
+def write(req):
+    if req.method == 'POST':
+        title = req.POST.get('title')
+        data = req.POST.get('field')
+        if not data:
+            return HttpResponse('NO DATA')
+
+
+    pass
+
+
+
+
+
+
+
